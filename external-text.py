688c2cec72f85da17427c233a1948a8a3f68290c
Ben <92220618+cfop-user@users.noreply.github.com>
2022-02-22 14:57:09 +0000

    Create external-text.py

---
+++ external-text.py
@@ -0,0 +1,12 @@
+keywords1 = ["start","help","see inventory"]
+meanings1 = ["Takes you to the start of the checkpoint.", "Gives you the key words and what they mean.","see stored items"]
+#stage2
+keywords2 = ["start","help","jump","walk","see inventory"]
+meanings2 = ["Takes you to the start of the checkpoint.", "Gives you the key words and what they mean.","lets you jump","you walk","see stored items"]
+#stage2.1
+keywords21 = ["start","help","pick it up","keep going","see inventory"]
+meanings21 = ["Takes you to the start of the checkpoint.", "Gives you the key words and what they mean.","pick up the stick","walk on without it","see stored items"]
+#stage 3
+keywords3 = allowed_answers = ["jump","walk","help","start","see inventory"]
+meanings3 = ["lets you jump","you walk further","Takes you to the start of the checkpoint.", "See stored items"]
+
